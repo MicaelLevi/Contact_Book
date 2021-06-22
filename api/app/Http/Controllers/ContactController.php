473d660b162b230fceaacf1aea3404515e7b37b2
MicaelLevi <micaellevins@gmail.com>
2021-06-22 11:59:10 -0300

    Built controller & htttp error handlers

---
+++ api/app/Http/Controllers/ContactController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Contact;
+class ContactController extends Controller
+{
+    public function index() {
+        return Contact::all();
+    }
+
+    public function store(Request $request) {
+        try {
+            $contact = Contact::create($request->all());
+        } catch (\Throwable $th) {
+            return response()->json(['message' => 'Não foi possivel salvar contato'], 500);
+        }
+        return response()->json($contact, 201);
+    }
+
+    public function update(Request $request, Contact $contact) {
+        
+        $contact->update($request->all());
+
+        return response()->json($contact, 200);
+    }
+
+    public function delete(Contact $contact) {
+        $contact->delete();
+
+        return response()->json(null, 204);
+    }
+}
