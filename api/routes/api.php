3bc20ba7ae2f598439451513cf7e50f1c3697ea3
MicaelLevi <micaellevins@gmail.com>
2021-06-22 11:56:14 -0300

    Built routes

--- api/routes/api.php
+++ api/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ContactController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +19,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/contacts', [ContactController::class, 'index']);
+
+Route::post('/contacts', [ContactController::class, 'store']);
+
+Route::put('/contacts/{contact}', [ContactController::class, 'update']);
+
+Route::delete('/contacts/{contact}', [ContactController::class, 'delete']);
