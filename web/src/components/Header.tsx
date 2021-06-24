5f63b9d78f1a0d4e1e08ed14899597f654e4f951
MicaelLevi <micaellevins@gmail.com>
2021-06-24 18:39:47 -0300

    Built page header

---
+++ web/src/components/Header.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { Button, AppBar, Toolbar } from '@material-ui/core';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import { NoEncryption } from '@material-ui/icons';
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            flexGrow: 1,
+            alignContent: "center"
+        },
+        menuButton: {
+            marginRight: theme.spacing(2),
+            textDecoration: "none",
+        },
+        appBar: {
+            alignItems: "center",
+        },
+    }),
+);
+
+function Header() {
+    const classes = useStyles();
+    return (
+        <AppBar position="static" className={classes.appBar}>
+            <Toolbar variant="dense" >
+                <NavLink exact activeClassName="active" to="/" style={{ textDecoration: 'none' }}>
+                    <Button variant="contained" color="primary" className={classes.menuButton}>
+                        Pagina Inicial
+                    </Button>
+
+                </NavLink>
+                <NavLink exact activeClassName="active" to="/adicionar" style={{ textDecoration: 'none' }}>
+                    <Button variant="contained" color="primary" className={classes.menuButton}>
+                        Adicionar Contato
+                    </Button>
+
+                </NavLink>
+            </Toolbar>
+        </AppBar>
+
+
+    )
+};
+
+export default Header;
